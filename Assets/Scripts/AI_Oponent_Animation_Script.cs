19ce1d4ff58e50b074071a2daf34e0502447c165
KingKutza <kingkutzathefirst@gmail.com>
2024-02-22 12:41:14 -0500

    Enemey Motion sorta working, in Test Scene

--- Assets/Scripts/AI_Oponent_Animation_Script.cs
+++ Assets/Scripts/AI_Oponent_Animation_Script.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class AI_Oponent_Animation_Script : MonoBehaviour
 {
+
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool("Is_Idle",true);
     }
 }
