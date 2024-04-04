045a8965e4a2f58431d73dfba0a416dd406ca203
KingKutza <kingkutzathefirst@gmail.com>
2024-04-04 12:44:05 -0400

    Added basic components to the Social scene

---
+++ Assets/Scripts/Social_Dialouge_manager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Social_Dialouge_manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+//TODO import Dialouge doc
+//Reference NPC_Dialouge textbox
+//Reference PC_Dialouge Textbox
+//Update the contents of NPC_Dialouge Textbox
