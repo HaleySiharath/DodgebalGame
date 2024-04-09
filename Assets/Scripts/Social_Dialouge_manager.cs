8f78879cc11bc534abd20a2e2a3dbe0fac851179
KingKutza <kingkutzathefirst@gmail.com>
2024-04-09 12:48:40 -0400

    We have Dialogue in the Social Scene!

--- Assets/Scripts/Social_Dialouge_manager.cs
+++ Assets/Scripts/Social_Dialouge_manager.cs
@@ -1,24 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.IO;
 
-public class Social_Dialouge_manager : MonoBehaviour
-{
+public class Social_Dialouge_manager : MonoBehaviour{
+    public TMP_Text NPCDialogueText;
+    public TMP_Text PCDialogueText;
+    private string[] DialogueFile;
+    private int n = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        string filePath = Path.Combine(Application.dataPath, "Dialogue/Dialogue_Template.txt");
+        if(File.Exists(filePath)){
+            DialogueFile = File.ReadAllLines(filePath);
+        }
+    
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if (Input.GetMouseButtonDown(0)){ // 0 is the button number for left click
+            ChangeText();
+        }
     }
+
+    void ChangeText(){
+        if (DialogueFile != null){
+            if(n >= DialogueFile.Length){
+                n = 0;
+            }
+            NPCDialogueText.text = "NPC says: " + DialogueFile[n];
+            PCDialogueText.text = "PC says: " + DialogueFile[n];
+            n++;
+        }
+
+
+
+    }
+
 }
 
-
-//TODO import Dialouge doc
-//Reference NPC_Dialouge textbox
-//Reference PC_Dialouge Textbox
-//Update the contents of NPC_Dialouge Textbox
+// TODO Enter the Dialogue fill into an object that can return appropriate text for each NPC / PC
+// TODO Update GUI to allow PC to Select Text Option
+// TODO Integrate
+// TODO Merge into Main Game
