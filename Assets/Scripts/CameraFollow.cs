b2d76047768c137e0c1c213779192f936fe89db2
PixeLilly75 <wdstewartfree@gmail.com>
2024-04-19 17:59:14 -0400

    Readded Cinemachine Package, added camFollowObj and have a working camera for the player in the social scene

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,20 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    [SerializeField] private Vector3 cameraOffset;
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (SceneManager.GetActiveScene() == "SocialScene") {Incorporate the following}
-        //Change camera behavior for different scenes
-
-        transform.position = player.position + cameraOffset;
-        
-        //revolve around player behind their back
-        //if something cuts distance from camera to player, jump to wall position
-        //Try Quanternion.Euler Later
-
-    }
+    
 }
